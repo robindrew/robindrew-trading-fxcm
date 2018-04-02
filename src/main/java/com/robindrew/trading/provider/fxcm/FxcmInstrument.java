3acddb6e71d09a49718218b6bb0621ac22b59d2a
Robin Drew <robin.drew@gmail.com>
2018-04-03 00:49:14 +0100

    +FxcmInstrument, Parsing for tick files

--- src/main/java/com/robindrew/trading/provider/fxcm/FxcmInstrument.java
+++ src/main/java/com/robindrew/trading/provider/fxcm/FxcmInstrument.java
@@ -1,5 +1,135 @@
 package com.robindrew.trading.provider.fxcm;
 
-public class FxcmInstrument {
+import static com.robindrew.trading.Instruments.AUD_CAD;
+import static com.robindrew.trading.Instruments.AUD_CHF;
+import static com.robindrew.trading.Instruments.AUD_JPY;
+import static com.robindrew.trading.Instruments.AUD_NZD;
+import static com.robindrew.trading.Instruments.CAD_CHF;
+import static com.robindrew.trading.Instruments.EUR_AUD;
+import static com.robindrew.trading.Instruments.EUR_CHF;
+import static com.robindrew.trading.Instruments.EUR_GBP;
+import static com.robindrew.trading.Instruments.EUR_JPY;
+import static com.robindrew.trading.Instruments.EUR_USD;
+import static com.robindrew.trading.Instruments.GBP_CHF;
+import static com.robindrew.trading.Instruments.GBP_JPY;
+import static com.robindrew.trading.Instruments.GBP_NZD;
+import static com.robindrew.trading.Instruments.GBP_USD;
+import static com.robindrew.trading.Instruments.NZD_CAD;
+import static com.robindrew.trading.Instruments.NZD_CHF;
+import static com.robindrew.trading.Instruments.NZD_JPY;
+import static com.robindrew.trading.Instruments.NZD_USD;
+import static com.robindrew.trading.Instruments.USD_CAD;
+import static com.robindrew.trading.Instruments.USD_CHF;
+import static com.robindrew.trading.Instruments.USD_JPY;
+
+import com.robindrew.trading.IInstrument;
+import com.robindrew.trading.Instrument;
+import com.robindrew.trading.price.precision.IPricePrecision;
+import com.robindrew.trading.price.precision.PricePrecision;
+
+public class FxcmInstrument extends Instrument {
+
+	/** AUD/CAD. */
+	public static final FxcmInstrument AUDCAD = new FxcmInstrument("AUDCAD", AUD_CAD, 6, 40000, 190000);
+	/** AUD/CHF. */
+	public static final FxcmInstrument AUDCHF = new FxcmInstrument("AUDCHF", AUD_CHF, 6, 40000, 190000);
+	/** AUD/JPY. */
+	public static final FxcmInstrument AUDJPY = new FxcmInstrument("AUDJPY", AUD_JPY, 6, 40000, 190000);
+	/** AUD/NZD. */
+	public static final FxcmInstrument AUDNZD = new FxcmInstrument("AUDNZD", AUD_NZD, 6, 40000, 190000);
+	/** CAD/CHF. */
+	public static final FxcmInstrument CADCHF = new FxcmInstrument("CADCHF", CAD_CHF, 6, 40000, 190000);
+	/** EUR/AUD. */
+	public static final FxcmInstrument EURAUD = new FxcmInstrument("EURAUD", EUR_AUD, 6, 40000, 190000);
+	/** EUR/CHF. */
+	public static final FxcmInstrument EURCHF = new FxcmInstrument("EURCHF", EUR_CHF, 6, 40000, 190000);
+	/** EUR/GBP. */
+	public static final FxcmInstrument EURGBP = new FxcmInstrument("EURGBP", EUR_GBP, 6, 40000, 190000);
+	/** EUR/JPY. */
+	public static final FxcmInstrument EURJPY = new FxcmInstrument("EURJPY", EUR_JPY, 6, 40000, 190000);
+	/** EUR/USD. */
+	public static final FxcmInstrument EURUSD = new FxcmInstrument("EURUSD", EUR_USD, 6, 40000, 190000);
+	/** GBP/CHF. */
+	public static final FxcmInstrument GBPCHF = new FxcmInstrument("GBPCHF", GBP_CHF, 6, 40000, 190000);
+	/** GBP/JPY. */
+	public static final FxcmInstrument GBPJPY = new FxcmInstrument("GBPJPY", GBP_JPY, 6, 40000, 190000);
+	/** GBP/NZD. */
+	public static final FxcmInstrument GBPNZD = new FxcmInstrument("GBPNZD", GBP_NZD, 6, 40000, 190000);
+	/** GBP/USD. */
+	public static final FxcmInstrument GBPUSD = new FxcmInstrument("GBPUSD", GBP_USD, 6, 40000, 190000);
+	/** NZD/CAD. */
+	public static final FxcmInstrument NZDCAD = new FxcmInstrument("NZDCAD", NZD_CAD, 6, 40000, 190000);
+	/** NZD/CHF. */
+	public static final FxcmInstrument NZDCHF = new FxcmInstrument("NZDCHF", NZD_CHF, 6, 40000, 190000);
+	/** NZD/JPY. */
+	public static final FxcmInstrument NZDJPY = new FxcmInstrument("NZDJPY", NZD_JPY, 6, 40000, 190000);
+	/** NZD/USD. */
+	public static final FxcmInstrument NZDUSD = new FxcmInstrument("NZDUSD", NZD_USD, 6, 40000, 190000);
+	/** USD/CAD. */
+	public static final FxcmInstrument USDCAD = new FxcmInstrument("USDCAD", USD_CAD, 6, 40000, 190000);
+	/** USD/CHF. */
+	public static final FxcmInstrument USDCHF = new FxcmInstrument("USDCHF", USD_CHF, 6, 40000, 190000);
+	/** USD/JPY. */
+	public static final FxcmInstrument USDJPY = new FxcmInstrument("USDJPY", USD_JPY, 6, 40000, 190000);
+
+	public static FxcmInstrument valueOf(String name) {
+		switch (name) {
+			case "AUDCAD":
+				return AUDCAD;
+			case "AUDCHF":
+				return AUDCHF;
+			case "AUDJPY":
+				return AUDJPY;
+			case "AUDNZD":
+				return AUDNZD;
+			case "CADCHF":
+				return CADCHF;
+			case "EURAUD":
+				return EURAUD;
+			case "EURCHF":
+				return EURCHF;
+			case "EURGBP":
+				return EURGBP;
+			case "EURJPY":
+				return EURJPY;
+			case "EURUSD":
+				return EURUSD;
+			case "GBPCHF":
+				return GBPCHF;
+			case "GBPJPY":
+				return GBPJPY;
+			case "GBPNZD":
+				return GBPNZD;
+			case "GBPUSD":
+				return GBPUSD;
+			case "NZDCAD":
+				return NZDCAD;
+			case "NZDCHF":
+				return NZDCHF;
+			case "NZDJPY":
+				return NZDJPY;
+			case "NZDUSD":
+				return NZDUSD;
+			case "USDCAD":
+				return USDCAD;
+			case "USDCHF":
+				return USDCHF;
+			case "USDJPY":
+				return USDJPY;
+			default:
+				throw new IllegalArgumentException("Unknown instrument: '" + name + "'");
+		}
+	}
+
+	private final IPricePrecision precision;
+
+	public FxcmInstrument(String name, IInstrument underlying, int decimalPlaces, int minPrice, int maxPrice) {
+		super(name, underlying);
+		this.precision = new PricePrecision(decimalPlaces, minPrice, maxPrice);
+	}
+
+	public IPricePrecision getPricePrecision() {
+		return precision;
+	}
 
 }
---
+++ src/main/java/com/robindrew/trading/provider/fxcm/data/FxcmTickFile.java
@@ -0,0 +1,130 @@
+package com.robindrew.trading.provider.fxcm.data;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Stopwatch;
+import com.robindrew.common.date.Dates;
+import com.robindrew.common.text.Strings;
+import com.robindrew.common.text.tokenizer.CharDelimiters;
+import com.robindrew.common.text.tokenizer.CharTokenizer;
+import com.robindrew.common.util.Java;
+import com.robindrew.trading.price.candle.IPriceCandleInstant;
+import com.robindrew.trading.price.candle.PriceCandleInstant;
+import com.robindrew.trading.price.candle.format.pcf.FloatingPoint;
+import com.robindrew.trading.provider.fxcm.FxcmInstrument;
+
+public class FxcmTickFile {
+
+	public static void main(String[] args) {
+		for (File file : new File("C:\\development\\data\\FXCM\\tick\\EURUSD\\").listFiles()) {
+			new FxcmTickFile(file, FxcmInstrument.EURUSD).readToList();
+		}
+	}
+
+	private static final Logger log = LoggerFactory.getLogger(FxcmTickFile.class);
+
+	private static DateTimeFormatter DATE_FORMAT = ofPattern("MM/dd/yyyy");
+	private static DateTimeFormatter TIME_FORMAT = ofPattern("HH:mm:ss.SSS");
+
+	public static final CharDelimiters DELIMITERS = new CharDelimiters().whitespace().character(',');
+
+	public static long toMillis(LocalDateTime date) {
+		return Dates.toMillis(date, ZoneOffset.UTC);
+	}
+
+	private final File file;
+	private final FxcmInstrument instrument;
+	/** Apparently they used UTF16 little endian. I wonder why? */
+	private Charset charset = Charsets.UTF_16LE;
+
+	public FxcmTickFile(File file, FxcmInstrument instrument) {
+		this.file = file;
+		this.instrument = instrument;
+	}
+
+	public List<IPriceCandleInstant> readToList() {
+		log.info("Reading ticks from {}", file.getName());
+		Stopwatch timer = Stopwatch.createStarted();
+		try (FileInputStream fileInput = new FileInputStream(file)) {
+			InputStream input = fileInput;
+
+			// GZIP file?
+			if (file.getName().endsWith(".gz")) {
+				input = new GZIPInputStream(fileInput);
+			}
+
+			// Read candles
+			List<IPriceCandleInstant> list = readToList(input);
+			timer.stop();
+			log.info("Read {} ticks from {} in {}", Strings.number(list), file.getName(), timer);
+			return list;
+
+		} catch (Exception e) {
+			throw Java.propagate(e);
+		}
+	}
+
+	private List<IPriceCandleInstant> readToList(InputStream input) throws IOException {
+		List<IPriceCandleInstant> list = new ArrayList<>();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input, charset));
+		while (true) {
+			String line = reader.readLine();
+			if (line == null) {
+				break;
+			}
+
+			IPriceCandleInstant instant = parseInstant(line);
+			if (instant != null) {
+				list.add(instant);
+			}
+		}
+
+		return list;
+	}
+
+	private IPriceCandleInstant parseInstant(String line) {
+		line = line.trim();
+		if (line.isEmpty() || line.startsWith("DateTime")) {
+			return null;
+		}
+
+		CharTokenizer tokenizer = new CharTokenizer(line, DELIMITERS);
+		int decimalPlaces = instrument.getPricePrecision().getDecimalPlaces();
+
+		// Dates
+		LocalDate date = LocalDate.parse(tokenizer.next(false), DATE_FORMAT);
+		LocalTime time = LocalTime.parse(tokenizer.next(false), TIME_FORMAT);
+		long timestamp = toMillis(LocalDateTime.of(date, time));
+
+		// Prices
+		BigDecimal bid = new BigDecimal(tokenizer.next(false));
+		BigDecimal ask = new BigDecimal(tokenizer.next(false));
+
+		int bidPrice = FloatingPoint.toBigInt(bid, decimalPlaces);
+		int askPrice = FloatingPoint.toBigInt(ask, decimalPlaces);
+
+		return new PriceCandleInstant(bidPrice, askPrice, timestamp, decimalPlaces);
+	}
+
+}
