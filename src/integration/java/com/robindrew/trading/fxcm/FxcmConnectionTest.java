949d6746d152da55354205d5ec942cb76a0ba889
Robin Drew <robin.drew@gmail.com>
2018-04-21 00:28:42 +0100

    Formatting

--- src/integration/java/com/robindrew/trading/fxcm/FxcmConnectionTest.java
+++ src/integration/java/com/robindrew/trading/fxcm/FxcmConnectionTest.java
@@ -4,21 +4,15 @@ import static com.robindrew.trading.fxcm.platform.FxcmEnvironment.DEMO;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 import java.io.File;
-import java.math.BigDecimal;
 
 import org.junit.Test;
 
-import com.robindrew.common.locale.CurrencyCode;
 import com.robindrew.common.util.Threads;
 import com.robindrew.trading.fxcm.platform.FxcmCredentials;
 import com.robindrew.trading.fxcm.platform.FxcmEnvironment;
 import com.robindrew.trading.fxcm.platform.FxcmSession;
 import com.robindrew.trading.fxcm.platform.rest.FxcmRestService;
-import com.robindrew.trading.fxcm.platform.rest.getopenpositions.FxcmPosition;
 import com.robindrew.trading.log.TransactionLog;
-import com.robindrew.trading.position.order.IPositionOrder;
-import com.robindrew.trading.position.order.PositionOrder;
-import com.robindrew.trading.trade.TradeDirection;
 
 public class FxcmConnectionTest {
 
@@ -41,20 +35,20 @@ public class FxcmConnectionTest {
 			System.out.println(name);
 		}
 
-//		FxcmInstrument instrument = FxcmInstrument.SPOT_EUR_USD;
-//		rest.getMarketDataSnapshot(instrument);
-//
-//		TradeDirection direction = TradeDirection.BUY;
-//		CurrencyCode currency = CurrencyCode.GBP;
-//		BigDecimal tradeSize = new BigDecimal("1000");
-//		IPositionOrder order = new PositionOrder(instrument, direction, currency, tradeSize, 0, 0);
-//		rest.openPosition(order);
-//
-//		Threads.sleep(10, SECONDS);
-//
-//		for (FxcmPosition position : rest.getPositions()) {
-//			rest.closePosition(position);
-//		}
+		// FxcmInstrument instrument = FxcmInstrument.SPOT_EUR_USD;
+		// rest.getMarketDataSnapshot(instrument);
+		//
+		// TradeDirection direction = TradeDirection.BUY;
+		// CurrencyCode currency = CurrencyCode.GBP;
+		// BigDecimal tradeSize = new BigDecimal("1000");
+		// IPositionOrder order = new PositionOrder(instrument, direction, currency, tradeSize, 0, 0);
+		// rest.openPosition(order);
+		//
+		// Threads.sleep(10, SECONDS);
+		//
+		// for (FxcmPosition position : rest.getPositions()) {
+		// rest.closePosition(position);
+		// }
 
 		Threads.sleep(10, SECONDS);
 	}
