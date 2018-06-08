abd4f97801d74e77fea0df74f5118d3efd0ec201
Robin Drew <robin.drew@gmail.com>
2018-06-08 23:06:51 +0100

    Updated transaction log

--- src/main/java/com/robindrew/trading/fxcm/platform/FxcmSession.java
+++ src/main/java/com/robindrew/trading/fxcm/platform/FxcmSession.java
@@ -4,16 +4,16 @@ import com.robindrew.common.util.Check;
 
 public class FxcmSession implements IFxcmSession {
 
-	private final FxcmCredentials credentials;
+	private final IFxcmCredentials credentials;
 	private final FxcmEnvironment environment;
 
-	public FxcmSession(FxcmCredentials credentials, FxcmEnvironment environment) {
+	public FxcmSession(IFxcmCredentials credentials, FxcmEnvironment environment) {
 		this.credentials = Check.notNull("credentials", credentials);
 		this.environment = Check.notNull("environment", environment);
 	}
 
 	@Override
-	public FxcmCredentials getCredentials() {
+	public IFxcmCredentials getCredentials() {
 		return credentials;
 	}
 
--- src/main/java/com/robindrew/trading/fxcm/platform/api/java/IFxcmJavaService.java
+++ src/main/java/com/robindrew/trading/fxcm/platform/api/java/IFxcmJavaService.java
@@ -2,8 +2,14 @@ package com.robindrew.trading.fxcm.platform.api.java;
 
 import com.fxcm.fix.pretrade.TradingSessionStatus;
 import com.robindrew.trading.fxcm.IFxcmInstrument;
+import com.robindrew.trading.fxcm.platform.IFxcmSession;
+import com.robindrew.trading.log.ITransactionLog;
 
 public interface IFxcmJavaService {
+
+	IFxcmSession getSession();
+
+	ITransactionLog getTransactionLog();
 
 	void login();
 
